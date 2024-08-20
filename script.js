c64386b4e0b6132ca63093647cee96c7863fe301
shaynalene <shaynalenep@gmail.com>
2024-08-20 14:23:50 +0800

    Added "push new book to array" function

---
+++ script.js
@@ -0,0 +1,28 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return title + ", " + author + ", " + pages + ", " + read;
+  };
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  console.table(myLibrary);
+}
+
+const addbookbtn = document.querySelector("#addbook");
+
+addbookbtn.addEventListener("click", function (e) {
+  const bookname = document.getElementById("Book").value;
+  const bookauthor = document.getElementById("Author").value;
+  const bookpages = document.getElementById("Page").value;
+  const bookread = document.getElementById("Read").value;
+  const newBook = new Book(bookname, bookauthor, bookpages, bookread);
+  console.log(newBook.info());
+  addBookToLibrary(newBook);
+});
